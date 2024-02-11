7225f8e261cc4a624dd515c8837282a8fcae498c
Borislav Hristov <bofengejn@gmail.com>
2024-02-11 16:16:53 +0200

    update calc()

--- TL-angular/src/app/input/input.component.ts
+++ TL-angular/src/app/input/input.component.ts
@@ -6,10 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./input.component.css']
 })
 export class InputComponent {
-  verticalSquares: number = 1;
-  horizontalSquares: number = 1;
+  verticalSquares: number = 2;
+  horizontalSquares: number = 2;
   inputData: Array<number> = [];
   Array = Array
+  avarageHeight: String = "";
 
   formatLabel(isVertical: boolean) {
     return (value: number): string => {
@@ -23,11 +24,41 @@ export class InputComponent {
   }
 
   calc() {
+    this.inputData = [];
     for(let i = 0; i < (this.verticalSquares+1)*(this.horizontalSquares+1); i++) {
       let inputField = document.getElementById(`H${i}`)
       let inputValue = (inputField as HTMLInputElement).value;
       this.inputData.push(Number(inputValue))
     }
     console.log(this.inputData)
+    this.calcHk()
   }
+
+  calcHk() {
+    let h1_1 = this.inputData[0];
+    let h1_2 = this.inputData[this.verticalSquares];
+    let h1_3 = this.inputData[((this.verticalSquares+1)*this.horizontalSquares)];
+    let h1_4 = this.inputData[this.inputData.length-1];
+    this.avarageHeight = `∑H₁ = ${h1_1} + ${h1_2} + ${h1_3} + ${h1_4} = ${h1_1+h1_2+h1_3+h1_4}`;
+
+    let h2_1 = 0;
+    let h2_2 = 0;
+    let h2_3 = 0;
+    let h2_4 = 0;
+
+    for(let i = 1; i < this.verticalSquares; i++){
+      h2_1 += this.inputData[i];
+    }
+
+    for(let i = ((this.verticalSquares+1)*this.horizontalSquares); i < this.inputData.length-1; i++){
+      h2_1 += this.inputData[i];
+    }
+    
+  }
+
+
+
+
+
+
 }
