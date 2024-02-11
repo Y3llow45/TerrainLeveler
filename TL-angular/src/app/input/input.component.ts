08f8e64ac97343002487b587135b69d1cfc6196f
Borislav Hristov <bofengejn@gmail.com>
2024-02-11 17:20:40 +0200

    Update calc

--- TL-angular/src/app/input/input.component.ts
+++ TL-angular/src/app/input/input.component.ts
@@ -10,6 +10,9 @@ export class InputComponent {
   horizontalSquares: number = 2;
   inputData: Array<number> = [];
   Array = Array
+  avarageH1: String = "";
+  avarageH2: String = "";
+  avarageH4: String = "";
   avarageHeight: String = "";
 
   formatLabel(isVertical: boolean) {
@@ -39,7 +42,7 @@ export class InputComponent {
     let h1_2 = this.inputData[this.verticalSquares];
     let h1_3 = this.inputData[((this.verticalSquares+1)*this.horizontalSquares)];
     let h1_4 = this.inputData[this.inputData.length-1];
-    this.avarageHeight = `∑H₁ = ${h1_1} + ${h1_2} + ${h1_3} + ${h1_4} = ${h1_1+h1_2+h1_3+h1_4}`;
+    this.avarageH1 = `∑H₁ = ${h1_1} + ${h1_2} + ${h1_3} + ${h1_4} = ${h1_1+h1_2+h1_3+h1_4}`;
 
     let h2_1 = 0;
     let h2_2 = 0;
@@ -49,11 +52,26 @@ export class InputComponent {
     for(let i = 1; i < this.verticalSquares; i++){
       h2_1 += this.inputData[i];
     }
-
-    for(let i = ((this.verticalSquares+1)*this.horizontalSquares); i < this.inputData.length-1; i++){
-      h2_1 += this.inputData[i];
+    for(let i = ((this.verticalSquares+1)*this.horizontalSquares)+1; i < this.inputData.length-1; i++){
+      h2_4 += this.inputData[i];
     }
+    for(let i = this.verticalSquares+1; i < ((this.verticalSquares+1)*this.horizontalSquares); i+=this.verticalSquares+1){
+      h2_2 += this.inputData[i];
+    }
+    for(let i = ((this.verticalSquares+1)*2)-1; i < this.inputData.length-1; i+=this.verticalSquares+1){
+      h2_3 += this.inputData[i];
+    }
+    this.avarageH2 = `∑H₂ = ${h2_1} + ${h2_2} + ${h2_3} + ${h2_4} = ${h2_1+h2_2+h2_3+h2_4}`;
     
+    let h4 = [];
+
+    for(let h = this.verticalSquares+2; h < (this.verticalSquares*2)+1; h++) {
+      for(let v = h; v < (this.verticalSquares+1)*this.horizontalSquares; v+=this.verticalSquares+1) {
+        h4.push(this.inputData[v])
+      }
+    }
+
+    this.avarageH4 = `∑H₄ = ${h4.join('+')} = ${h4.reduce((sum, value) => sum + value, 0)}`;
   }
 
 
