ff69ca21168cf0872744963e90dccc53139d930a
Borislav Hristov <bofengejn@gmail.com>
2024-02-12 20:06:28 +0200

    Update input.component.ts

--- TL-angular/src/app/input/input.component.ts
+++ TL-angular/src/app/input/input.component.ts
@@ -1,11 +1,11 @@
-import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.css']
 })
-export class InputComponent implements AfterViewInit {
+export class InputComponent {
   @ViewChild('myCanvas', { static: false }) canvas!: ElementRef<HTMLCanvasElement>;
 
   verticalSquares: number = 2;
@@ -23,11 +23,7 @@ export class InputComponent implements AfterViewInit {
   lineX2: number = 50;
   lineY2: number = 100;
 
-  ngAfterViewInit() {
-    this.drawRedLine();
-  }
-
-  drawRedLine() {
+  drawRedLine(a:number, b:number, c:number, d:number) {
     const canvas = this.canvas.nativeElement;
     if (!canvas) {
       console.error('Canvas element not found');
@@ -39,12 +35,11 @@ export class InputComponent implements AfterViewInit {
       return;
     }
     ctx.beginPath();
-    ctx.moveTo(50, 0); // replace with your actual values
-    ctx.lineTo(50, 100); // replace with your actual values
+    ctx.moveTo(a, b); // 50, 0
+    ctx.lineTo(c, d); // 50, 100
     ctx.strokeStyle = 'red';
     ctx.lineWidth = 2;
     ctx.stroke();
-
   }
 
   updateCanvas() {
@@ -131,9 +126,10 @@ export class InputComponent implements AfterViewInit {
 
     this.avarageH4 = `∑H₄ = ${h4.join('+')} = ${h4.reduce((sum, value) => sum + value, 0)}`;
     this.avarageHeight = `H₀ = ( ${h1_1+h1_2+h1_3+h1_4} + 2*${h2_1+h2_2+h2_3+h2_4} + 4*${h4.reduce((sum, value) => sum + value, 0)} ) / 4*${this.verticalSquares*this.horizontalSquares} =  ${((h1_1+h1_2+h1_3+h1_4+(2*(h2_1+h2_2+h2_3+h2_4))+(4*(h4.reduce((sum, value) => sum + value, 0))))/(4*this.verticalSquares*this.horizontalSquares)).toFixed(2)}`;
-    
-
+    this.drawZeroLine()
   }
 
-  
+  drawZeroLine() {
+    this.drawRedLine(85,0,170/2,120);
+  }
 }
